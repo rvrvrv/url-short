0aaf38920ee72486f03bd7d556b38add38d5bcf8
Rich <ibanez19@sbcglobal.net>
2017-01-26 17:51:37 +0000

    Heroku upload

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+//URL-shortening host
+config.webhost = 'https://url-shortener-rv.herokuapp.com/';
+
+//MongoDB host and database name
+config.urlDb = 'mongodb://accth3adm1n:test84231594235@ds131109.mlab.com:31109/url-shortener-rv';
+
+module.exports = config;
